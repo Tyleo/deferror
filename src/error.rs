64c0084d2e485e1d2714805a5880f78de0977e30
Tyler Wolf Leonhardt <tyler.wolf.leonhardt@gmail.com>
2016-05-15 09:49:10 +0100

    Added some convienience uses to the error macro.

--- src/error.rs
+++ src/error.rs
@@ -5,27 +5,32 @@ macro_rules! error {
             $($current_error_ident: ident),+
         }
     ) => {
+        use std::error::Error as DefineErrorError;
+        use std::fmt::Display as DefineErrorDisplay;
+        use std::fmt::Formatter as DefineErrorFormatter;
+        use std::fmt::Result as DefineErrorFmtResult;
+
         #[derive(Debug)]
         pub enum $main_error_ident {
             $($current_error_ident($current_error_ident)),+
         }
 
-        impl Display for $main_error_ident {
-            fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        impl DefineErrorDisplay for $main_error_ident {
+            fn fmt(&self, f: &mut DefineErrorFormatter) -> DefineErrorFmtResult {
                 match *self {
                     $($main_error_ident::$current_error_ident(ref err) => write!(f, "{}", err)),+
                 }
             }
         }
 
-        impl Error for $main_error_ident {
+        impl DefineErrorError for $main_error_ident {
             fn description(&self) -> &str {
                 match *self {
                     $($main_error_ident::$current_error_ident(ref err) => err.description()),+
                 }
             }
 
-            fn cause(&self) -> Option<&Error> {
+            fn cause(&self) -> Option<&DefineErrorError> {
                 match *self {
                     $($main_error_ident::$current_error_ident(ref err) => Some(err)),+
                 }
